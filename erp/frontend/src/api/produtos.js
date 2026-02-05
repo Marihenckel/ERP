c8f1dfd68688f4b0b086b4c844b1ae5c1f02e804
Mariana Henckel <marihenckelsdr@hotmail.com>
2026-02-05 17:03:02 -0300

    config api baseURL via env

--- erp/frontend/src/api/produtos.js
+++ erp/frontend/src/api/produtos.js
@@ -1,10 +1,18 @@
-import api from "../services/api";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+});
 
 export const listarProdutos = () => api.get("/produtos");
 
-export const criarProduto = (produto) => api.post("/produtos", produto);
+export const criarProduto = (produto) =>
+  api.post("/produtos", produto);
 
-export const atualizarProduto = (id, produto) => api.put(`/produtos/${id}`, produto);
+export const atualizarProduto = (id, produto) =>
+  api.put(`/produtos/${id}`, produto);
 
-export const deletarProduto = (id) => api.delete(`/produtos/${id}`);
+export const deletarProduto = (id) =>
+  api.delete(`/produtos/${id}`);
+
 
